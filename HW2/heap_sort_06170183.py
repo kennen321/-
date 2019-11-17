000d31e7edfa532a4fc9346e1fd975cd26f48b8b
kennen321 <52487493+kennen321@users.noreply.github.com>
2019-11-17 21:17:05 +0800

    Add files via upload

---
+++ HW2/heap_sort_06170183.py
@@ -0,0 +1,35 @@
+class Solution(object):
+ def heap_sort(self,arr):
+    import math
+    global arrLen
+    arrLen = len(arr)
+    for i in range(math.floor(len(arr)/2),-1,-1):
+        self.heapify(arr,i)
+    
+    
+    for i in range(len(arr)-1,0,-1):  
+        
+        arr[i], arr[0] = arr[0], arr[i]
+        arrLen -=1
+        self.heapify(arr, 0)
+    return arr
+
+ def heapify(self,arr,i):
+    
+    left = 2*i+1  
+    right = 2*i+2 
+    largest = i
+    
+    
+    if left < arrLen and arr[left] > arr[largest]:
+        largest = left
+    
+    if right < arrLen and arr[right] > arr[largest]:
+        largest = right
+    
+    if largest != i:
+        arr[largest], arr[i] = arr[i], arr[largest]
+        self.heapify(arr, largest)
+
+
+print(Solution().heap_sort([9, 3, 0, 0, 42, 1, -1,3,22,-50,42,-1 ]))
